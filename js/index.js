3be65755b0e6be3df474d9be84a7e2f6d9fb2537
Sylar Li <65619327+OwOsas@users.noreply.github.com>
2022-01-06 20:35:41 -0500

    Team Intro Init

---
+++ js/index.js
@@ -0,0 +1,46 @@
+const cards = document.querySelectorAll(".card");
+
+const options = {
+    root: null,
+    threshold: .525,
+    rootMargin: "-10%"
+};
+
+
+function is_touch_screen() {
+    return ( 'ontouchstart' in window ) || 
+           ( navigator.maxTouchPoints > 0 ) || 
+           ( navigator.msMaxTouchPoints > 0 );
+}
+
+console.log(document.body.clientHeight, window.innerHeight);
+console.log(document.body.clientHeight/window.innerHeight);
+
+function colorize_card(){
+    if(document.body.clientHeight/window.innerHeight > 1){
+        var observer = new IntersectionObserver(function(entries, observer){
+            entries.forEach(entry =>{
+                if(entry.isIntersecting){
+                    entry.target.classList.add("active");
+                    console.log(entry.target.id)
+                }
+                else{
+                    entry.target.classList.remove("active");
+                }
+        
+            })
+        },options)
+        
+        cards.forEach(function(card){
+            observer.observe(card)
+        })
+    }
+}
+
+window.addEventListener('resize', function(){
+    colorize_card();
+});
+
+window.addEventListener("load", function(){
+    colorize_card();
+});
